e11a928d3add552beb0a213da521c7a00066d39e
Leotor1157 <leo.tor1157@gmail.com>
2023-02-07 13:34:05 +0100

    compteur fonctionel en console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 console.log("Bonjour :)");
+
+const plus = document.querySelector('.plus')
+const moin = document.querySelector('.minus')
+const raz = document.querySelector('.reset')
+let cpt = 0
+
+
+
+function fplus(){
+    return cpt += 1
+}
+function fminus(cpt){
+    if (cpt != 0) 
+        cpt -= 1 
+    return cpt 
+} 
+    
+
+function freset() {
+    return cpt = 0
+}
+
+plus.addEventListener('click', () => {
+    console.log('plus')
+    cpt = fplus()
+    console.log(cpt)
+  })
+moin.addEventListener('click', () => {
+    console.log('minus')
+    cpt = fminus(cpt)
+    console.log(cpt)
+  })
+raz.addEventListener('click', () => {
+    console.log('reset')
+    cpt = freset()
+    console.log(cpt)
+  })
+
+
