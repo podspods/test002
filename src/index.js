42e72dc2ca72370d729aaf2230fbc7726976d7f3
Badreddine Hadjoudj <b.hadjoudj@live.fr>
2023-02-07 13:54:52 +0100

    fichier final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Bonjour :)");
+
+
+
+let count = 0;
+
+const plusButton = document.querySelector(".plus");
+plusButton.addEventListener("click", function () {
+    count++;
+    document.querySelector(".counter").innerHTML = count;
+});
+
+const minusButton = document.querySelector(".minus");
+minusButton.addEventListener("click", function () {
+    
+    if(count > 0){
+
+        count--;
+        document.querySelector(".counter").innerHTML = count;
+    }
+});
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", function () {
+    count = 0;
+    document.querySelector(".counter").innerHTML = count;
+});
+
+
+
