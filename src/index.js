ba9ee88ad642a7f25827022c16f25b18f5eb3cb5
Anyanka77 <ylasseaux@gmail.com>
2023-02-07 12:46:48 +0100

    modification de l'index.html, style.css, index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Bonjour :)");
+const counterEl = document.querySelector('.counter')
+const plusBtn = document.querySelector('.plus')
+const minusBtn = document.querySelector('.minus')
+const resetBtn = document.querySelector('.reset')
+let count = 0
+
+plusBtn.addEventListener('click', function () {
+  count++
+  counterEl.innerHTML = count
+})
+
+minusBtn.addEventListener('click', function () {
+  if (count > 0) {
+    count--
+    counterEl.innerHTML = count
+  }
+})
+
+resetBtn.addEventListener('click', function () {
+  count = 0
+  counterEl.innerHTML = count
+})
